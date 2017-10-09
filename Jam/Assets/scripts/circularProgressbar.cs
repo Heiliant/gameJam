491a38c9bdbd411637094ef256f5717a074d8dc9
Alex Balastegui Gaya <abalas98@hotmail.com>
2017-10-09 23:58:57 +0200

    First commit
    Player and camera movement.
    The camera stops as soon as the user stops taping the player.
    I have also added the timer.

---
+++ Jam/Assets/scripts/circularProgressbar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class circularProgressbar : MonoBehaviour
+{
+    private Image myImage;
+
+    private float progression;
+
+    public void setProgression(float num) {
+        while (num > 1.0f)
+            num--;
+        progression = num;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        myImage = GetComponent<Image>();
+        myImage.type = UnityEngine.UI.Image.Type.Filled;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myImage.fillAmount = progression;
+    }
+}
---
+++ Jam/Assets/scripts/mouseFollower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseFollower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Transform>().position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y, 0.0f);
+    }
+}
