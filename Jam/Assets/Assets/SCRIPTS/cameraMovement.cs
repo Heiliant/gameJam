a00c10cfd1b7def653e594e606b3d4ac3d1056ed
Heiliant <alexbalasteguigaya@enti.cat>
2017-10-10 09:14:15 +0200

    triangulo demierda
    jj

---
+++ Jam/Assets/Assets/SCRIPTS/cameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+
+    public float speed=1.0f;
+    private Transform me;
+    
+
+	// Use this for initialization
+	void Start () {
+        me = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        me.Translate(new Vector2(0.0f, Time.deltaTime * speed * 100.0f));
+	}
+}
---
+++ Jam/Assets/Assets/SCRIPTS/pjMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pjMovement : MonoBehaviour {
+    private float rad = 0.64f;
+    private Vector3 me;
+
+    public float bigRad; //rad of the detection once the player is moving, so of it moves kinda fast, the object doesnt lose track of the finger
+    public float littleRad;//the rad of the actual circle
+
+    public float camSpeedReleased;
+    public float camSpeedPressed;
+
+    TargetJoint2D myJoint;
+
+    camBehaviour myCamera;
+    timerBehaviour myTimer;
+
+    // Use this for initialization
+    void Start () {
+        myJoint = GetComponent<TargetJoint2D>();
+        myJoint.autoConfigureTarget = false;
+
+        myCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<camBehaviour>();
+        myTimer = GameObject.Find("timer").GetComponent<timerBehaviour>();
+
+        myCamera.Speed = camSpeedReleased;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 myMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        me = GetComponent<Transform>().position;
+        
+        if (Input.GetMouseButton(0) && Vector2.Distance(me, myMouse) < rad)
+        {
+            //the camera moves
+            rad = bigRad;
+            myJoint.target = myMouse;
+            myCamera.Speed = camSpeedPressed;
+
+            myTimer.toCountdown = false;
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //the camera stops moving
+            rad = littleRad;
+            myCamera.Speed = camSpeedReleased;
+
+            myTimer.toCountdown = true;
+        }
+    }
+}
